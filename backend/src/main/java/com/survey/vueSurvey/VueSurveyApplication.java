a329db844c3f7fb4069aaf8dd503327d9a76d66a
이경용 <sleekydz86@naver.com>
2023-04-08 16:09:14 +0900

    beackend셋팅
    backend셋팅

---
+++ backend/src/main/java/com/survey/vueSurvey/VueSurveyApplication.java
@@ -0,0 +1,13 @@
+package com.survey.vueSurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSurveyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/survey/vueSurvey/VueSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.survey.vueSurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueSurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
